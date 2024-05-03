92ce55f123931d98797b11ccc30de32af3644b20
Logan Woolf <loganwoolf@gmail.com>
2024-05-03 02:04:00 -0600

    Disable generator tests

--- src/utilities/generate.test.ts
+++ src/utilities/generate.test.ts
@@ -1,17 +1,15 @@
-import { expect, test } from "bun:test";
-import { createFunctions, createTests } from "./generate";
+// import { expect, test } from "bun:test";
+// import { createFunctions, createTests } from "./generate";
 
-test("createFunctions", () => {
-	const functionNames = "add,subtract, multiply";
-	const outputString = createFunctions(functionNames);
-	console.log({ outputString });
-	expect(outputString).toContain("add");
-});
+// test.skip("createFunctions", () => {
+// 	const functionNames = "add,subtract, multiply";
+// 	const outputString = createFunctions(functionNames);
+// 	expect(outputString).toContain("add");
+// });
 
-test("createTests", () => {
-	const fileName = "test";
-	const functionNames = "add,subtract, multiply";
-	const outputString = createTests(fileName, functionNames);
-	console.log({ outputString });
-	expect(outputString).toContain("add");
-});
+// test.skip("createTests", () => {
+// 	const fileName = "test";
+// 	const functionNames = "add,subtract, multiply";
+// 	const outputString = createTests(fileName, functionNames);
+// 	expect(outputString).toContain("add");
+// });
